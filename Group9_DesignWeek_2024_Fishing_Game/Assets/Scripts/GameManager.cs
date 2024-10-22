0b70614ce9e018332b30bf1388a18d45b23f0d33
Neil-McCuaig <Neil_McCuaig@bell.net>
2024-10-22 19:34:39 -0400

    Added Timer and Game Over
    Added a simple five minute timer, a UI display for that timer, and a simple game over screen with a restart button.

---
+++ Group9_DesignWeek_2024_Fishing_Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GameManager : MonoBehaviour
+{
+    //300 for five minutes. Set it to 10 for a ten second test.
+    public float timeRemaining = 300f;
+
+
+
+    public TextMeshProUGUI timerText;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timerText.text = "Time: "; DisplayTime(timeRemaining);
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+}
